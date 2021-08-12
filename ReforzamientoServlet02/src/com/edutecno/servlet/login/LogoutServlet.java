b509a33f64558529e3b7dec4a60ec93c89e8e34f
j0r3edu <68802810+j0r3edu@users.noreply.github.com>
2021-08-12 17:53:17 -0400

    Se suben proyectos

---
+++ ReforzamientoServlet02/src/com/edutecno/servlet/login/LogoutServlet.java
@@ -0,0 +1,36 @@
+package com.edutecno.servlet.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+		request.getRequestDispatcher("enlaces.html").include(request, response);
+
+		Cookie ck = new Cookie("name", "");
+		ck.setMaxAge(0);
+		response.addCookie(ck);
+
+		out.print("Deslogueado correctamente!");
+	}
+
+}
